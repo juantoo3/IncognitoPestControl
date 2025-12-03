bb41847fbef3a78da08a9575908b9aa78317c4b4
juantoo3 <49548148+juantoo3@users.noreply.github.com>
2025-12-03 13:35:27 +0800

    Delete navbar.ts

--- navbar.ts
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('header')!.innerHTML += data;
-        });
-});
